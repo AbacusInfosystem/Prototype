1ef86b2cb2823cdc12681d9278c8e139fa0b2dde
kaustubh-chaudhari <kaustubh.chaudhari@abacusinfosystem.com>
2016-11-18 19:05:57 +0530

    free

---
+++ Lohana/Scripts/generic-methods.js
@@ -0,0 +1,106 @@
+﻿
+function ToJavaScriptDate(value) {
+    if (value != "null" && value != null && value != "") {
+        var pattern = /Date\(([^)]+)\)/;
+        var results = pattern.exec(value);
+        var dt = new Date(parseFloat(results[1]));
+
+        var dtDate = dt.getDate();
+        if (dtDate.toString().length == 1) {
+            dtDate = "0" + dtDate;
+        }
+
+        var dtMonth = dt.getMonth() + 1;
+        if (dtMonth.toString().length == 1) {
+            dtMonth = "0" + dtMonth;
+        }
+
+        // return (dt.getMonth() + 1) + "/" + dtDate + "/" + dt.getFullYear();
+        //return dtMonth + "/" + dtDate + "/" + dt.getFullYear();
+        return dtDate + "/" + dtMonth + "/" + dt.getFullYear();
+    }
+    else {
+        return "";
+    }
+}
+
+function Bind_Images() {
+    $("#ShowImages").html("");
+
+    $.ajax({
+        type: "POST",
+        url: '/upload-file/get-browsed-images',
+        data: { File_Type: $("#hdnFileType").val() },
+        success: function (attachments) {
+
+            var htmlText = "";
+
+            if (attachments.length > 0) {
+
+                for (i = 0; i < attachments.length; i++) {
+
+                    htmlText += "<div id='DivImages' class='col-md-3' style='margin-top: 10px;'>";
+
+                    htmlText += "<div class='thumbnail panel'>";
+
+                    htmlText += "<input type='radio' name='Image' id='" + attachments[i].Attachment_Id + "' class='iradio-list' data-src='" + attachments[i].Unique_Id + "' />";
+
+                    //htmlText += "<label>" + attachments [i].File_Name + "</label>";
+
+                    htmlText += "<img style='width:130px; height: 113px;border: 1px solid;margin-left: auto;margin-right: auto;display: block;max-width: 100%;max-height: 100%;' src='" + attachments[i].Unique_Id + "'>"
+
+                    htmlText += "</div>";
+
+                    htmlText += "</div>";
+
+                }
+
+            }
+            //else {
+
+            //}
+
+            $('#ShowImages').append(htmlText);
+
+            $('.iradio-list').iCheck({
+                radioClass: 'iradio_square-green',
+                increaseArea: '20%'
+            });
+
+            $("#ImageModal").modal("show");
+
+            $('[name="Image"]').on('ifChanged', function (event) {
+                if ($(this).prop('checked')) {
+
+                    $("#hdnAttachmentId").val(this.id);
+                    var src = $(this).attr('data-src');
+                    $("#imgHeaderImage").attr('src', src);
+                    $("#hdnHeader_Image_Url").val(src);
+                }
+            });
+
+        }
+    });
+
+}
+
+
+function Get_Date_Month_Name(mon) {
+
+    var month = new Array();
+    month[0] = "Jan";
+    month[1] = "Feb";
+    month[2] = "Mar";
+    month[3] = "Apr";
+    month[4] = "May";
+    month[5] = "Jun";
+    month[6] = "Jul";
+    month[7] = "Aug";
+    month[8] = "Sep";
+    month[9] = "Oct";
+    month[10] = "Nov";
+    month[11] = "Dec";
+
+    var n = month[(parseInt(mon) - 1)];
+    return n;
+}
---
+++ Lohana/Scripts/generic.custom.validation.js
@@ -0,0 +1,137 @@
+﻿$(function () {
+
+    $.validator.setDefaults({
+        ignore: [],
+        errorElement: "span",
+        errorClass: "help-block",
+        highlight: function (element, errorClass, validClass) {
+
+            $(element).closest('.form-group').addClass('has-error');
+            // 
+            $(element).parent().addClass('has-error');
+            //
+            $(element).closest('.form-group').find('.input-group-addon').css({ 'color': '#A94442', 'background-color': '#F2DEDE', 'border-color': '#A94442' });
+
+            if ($(element).closest('.form-group').find(".ignore").prop("class") != undefined)
+            {
+                if($(element).closest('.form-group').find(".ignore").prop("class").indexOf("ignore") != -1)
+                {
+                    $(element).parent().removeClass('has-error');
+
+                    $(element).parent().addClass('has-warning');
+                }
+            }
+        },
+        unhighlight: function (element, errorClass, validClass) {
+            $(element).closest('.form-group').removeClass('has-error');
+            // 
+            $(element).parent().removeClass('has-error'); 
+            //
+            $(element).closest('.form-group').find('.input-group-addon').css({ 'color': 'black', 'background-color': '#FFF', 'border-color': '#D2D6DE' });
+
+            $(element).parent().removeClass('has-warning');
+        },
+        errorPlacement: function (error, element) {
+            if (element.parent('.input-group').length || element.prop('type') === 'checkbox' || element.prop('type') === 'radio') {
+                error.insertAfter(element.parent());
+            }
+                //
+            else if (element.parent('.auto-complete').length)
+            {
+                if (element.parent('.auto-complete').parent('.input-group').length) {
+                    error.insertAfter(element.parent('.auto-complete').parent());
+                }
+                else
+                {
+                    error.insertAfter(element.parent('.auto-complete'));
+                }
+            }
+             //
+            else {
+                error.insertAfter(element);
+            }
+        },
+
+    	onkeyup: false,
+
+    	onclick: false,
+    	
+
+    });
+
+    jQuery.validator.addMethod("validate_telephone_number", function (value, element) {
+        var result = false;
+
+        var tnre = /^\+?[0-9-]+$/;
+        var val = $(element).val();
+
+        if (tnre.test(val))
+        {
+            result = true;
+        }
+
+        if ($(element).val() == "")
+        {
+        	result = true;
+        }
+
+        return result;
+
+    }, "Please enter a valid phone number.");
+
+    jQuery.validator.addMethod("validate_fax_number", function (value, element) {
+        var result = false;
+
+        var tnre = /^\+?[0-9-]+$/;
+        var val = $(element).val();
+
+        if (tnre.test(val)) {
+            result = true;
+        }
+
+        return result;
+
+    }, "Please enter a valid fax number.");
+
+    jQuery.validator.addMethod("validate_file_size", function (value, element) {
+        var result = true;
+
+        if ($(element).data('max-size') != undefined)
+            if ($(element)[0].files[0].size > $(element).data('max-size') * (1024 * 1024))
+                return false;
+
+        return result;
+    });
+
+});
+
+function Set_Mandetory_Validation(name) {
+    $("[name='" + name + "']").rules("add", {
+        required: true
+    });
+}
+
+function Set_Email_Validation(name) {
+    $("[name='" + name + "']").rules("add", {
+        email: true,
+    });
+}
+
+function Set_Telephone_Validation(name) {
+    $("[name='" + name + "']").rules("add", { validate_telephone_number: true });
+}
+
+function Set_Fax_Validation(name) {
+    $("[name='" + name + "']").rules("add", { validate_fax_number: true });
+}
+
+function Remove_Validations(name) {
+    $("[name='" + name + "']").rules("remove");
+}
+
+function Set_File_Size_Validation(name) {
+    $("[name='" + name + "']").rules("add", {
+        validate_file_size: true, messages: { validate_file_size: "File size should not be greater than " + $("[name='" + name + "']").data('max-size') + " MB." }
+    });
+}
+
