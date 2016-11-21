400b5f225543a3e997338390ea880c9a693b037d
sushant-maragaje <sushant.maragaje4@gmail.com>
2016-11-21 20:24:33 +0530

    Enquiry Master Changes

---
+++ Lohana/Controllers/QuotationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lohana.Controllers
+{
+    public class QuotationController : Controller
+    {
+        //
+        // GET: /Quotation/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+    }
+}
